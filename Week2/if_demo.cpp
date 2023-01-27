ceb7c96ca8b462ee84837f6e85bd7b642b1a1b1d
Matsuo <35791279+matsuot-cu@users.noreply.github.com>
2023-01-27 07:04:47 -0700

    Add if_demo code

---
+++ Week2/if_demo.cpp
@@ -0,0 +1,24 @@
+/* ASEN 1320 demo code
+   Author: Tomoko Matsuo
+   Purpose: Demostrate how to if statement 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int x;
+   cout << "Enter number (int): "; 
+   cin >> x;  // User input example 1320
+
+   if (x == 100) {
+      cout << " x is 100";
+   } 
+   
+   /*else
+   {
+      cout << " x is not 100";
+   }*/
+  
+  return 0;
+}
