c44b162e1e53dfd15e72dad824810346a6dc5400
Akash Ratheesh <akashratheesh@gmail.com>
2023-02-03 08:00:58 -0700

    Coding Challenge Feb 3

---
+++ Week3/coding_challenge_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int count;
+    int sum = 0;
+
+    cout << "Enter a starting number: ";
+    cin >> count;
+
+
+    // Fill Loop Loginc
+    // Find the sum from 'count' to 1000
+
+
+
+
+
+
+    // END of LOOP
+
+
+
+    cout << "Sum is " << sum << endl;
+
+
+    return 0;
+}
