4260ce3874a549d7c3b8e20ab631321b9847ff96
Matsuo <35791279+matsuot-cu@users.noreply.github.com>
2023-02-10 06:28:13 -0700

    Add week.cpp demo code

---
+++ Week4/week.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+
+for (int week=1; week <= 4; week++)
+{
+    cout << "===Week" << week << "===" << endl;
+  
+    for (int days = 1; days <= 7; days++)
+    {
+       cout << "day " << days << endl;
+    }
+}   
+    return 0; 
+}
